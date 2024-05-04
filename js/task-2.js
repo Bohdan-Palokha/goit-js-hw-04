388b34ce365ba4aa0b1a5af97d4c8abc4ddf08b3
Bohdan-Palokha <palohabogdan21@gmail.com>
2024-05-04 20:10:24 +0300

    save

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,12 @@
-function calcAverageCalories(days) {
-    let totalCalories = 0;
-    for (let day of days) {
-        totalCalories += day.calories;
+const calcAverageCalories = days => {
+    if (days.length === 0) {
+        return 0;
     }
+    const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
     const averageCalories = totalCalories / days.length;
 
-    return averageCalories;
-}
+    return Math.round(averageCalories);
+};
 
 console.log(
     calcAverageCalories([
